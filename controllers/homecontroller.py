20347de30ba0d6d8963f20c080a82bd3f6c3cd78
Silvia <sarnaizgarcia@gmail.com>
2019-11-01 13:18:38 +0100

    Añadida opción de salir de navegación

--- controllers/homecontroller.py
+++ controllers/homecontroller.py
@@ -7,12 +7,13 @@ class HomeController(IController):
     def render(self):
         result = None
         valid_answers = {'1': 'controllers.loginuserncontroller',
+                         'S': None
                          }
         header = MainHeader()
         body = HomeView(header.renderbody())
 
         print(body.renderbody())
-        action = input(body.rendernavigation())
+        action = input(body.rendernavigation()).upper()
 
         if action in valid_answers:
             result = valid_answers[action]
